36c3c0480e25e6cb0bda8e35428bc899e038cee0
Michael Blisner <Michael.Blisner@telekom.de>
2020-01-14 09:48:48 +0100

    loesung

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -13,45 +13,60 @@ import com.wildcodeschool.wildandwizard.util.JdbcUtils;
 
 public class SchoolRepository {
 
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
-    private final static String DB_USER = "h4rryp0tt3r";
-    private final static String DB_PASSWORD = "Horcrux4life!";
+	private final static String DB_URL = "jdbc:mysql://localhost:3306/spring_jdbc_quest?serverTimezone=GMT";
+	private final static String DB_USER = "h4rryp0tt3r";
+	private final static String DB_PASSWORD = "Horcrux4life!";
 
-    public void deleteById(Long id) {
-        // TODO: delete a school from the database
-    }
+	public void deleteById(Long id) {
 
-    public List<School> findAll() {
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			statement = connection.prepareStatement("DELETE FROM spring_jdbc_quest.school WHERE id=?");
+			statement.setLong(1, id);
 
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = DriverManager.getConnection(
-                    DB_URL, DB_USER, DB_PASSWORD
-            );
-            statement = connection.prepareStatement(
-                    "SELECT * FROM school;"
-            );
-            resultSet = statement.executeQuery();
+			if (statement.executeUpdate() != 1) {
+				throw new SQLException("failed to delete data");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.closeResultSet(resultSet);
+			JdbcUtils.closeStatement(statement);
+			JdbcUtils.closeConnection(connection);
+		}
 
-            List<School> schools = new ArrayList<>();
+	}
 
-            while (resultSet.next()) {
-                Long id = resultSet.getLong("id");
-                String name = resultSet.getString("name");
-                Long capacity = resultSet.getLong("capacity");
-                String country = resultSet.getString("country");
-                schools.add(new School(id, name, capacity, country));
-            }
-            return schools;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            JdbcUtils.closeResultSet(resultSet);
-            JdbcUtils.closeStatement(statement);
-            JdbcUtils.closeConnection(connection);
-        }
-        return null;
-    }
+	public List<School> findAll() {
+
+		Connection connection = null;
+		PreparedStatement statement = null;
+		ResultSet resultSet = null;
+		try {
+			connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			statement = connection.prepareStatement("SELECT * FROM school;");
+			resultSet = statement.executeQuery();
+
+			List<School> schools = new ArrayList<>();
+
+			while (resultSet.next()) {
+				Long id = resultSet.getLong("id");
+				String name = resultSet.getString("name");
+				Long capacity = resultSet.getLong("capacity");
+				String country = resultSet.getString("country");
+				schools.add(new School(id, name, capacity, country));
+			}
+			return schools;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.closeResultSet(resultSet);
+			JdbcUtils.closeStatement(statement);
+			JdbcUtils.closeConnection(connection);
+		}
+		return null;
+	}
 }
